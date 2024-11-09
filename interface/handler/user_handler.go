2d1391973c80c3fc8d9c79df26a5789dc22329fb
takumines <takumines1@gmail.com>
2024-11-09 13:23:47 +0900

    [update] 不要なコメント削除

--- interface/handler/user_handler.go
+++ interface/handler/user_handler.go
@@ -1,4 +1,4 @@
-package handlers
+package handler
 
 import (
 	"github.com/labstack/echo/v4"
--- main.go
+++ main.go
@@ -4,16 +4,13 @@ import (
 	"github.com/labstack/echo/v4"
 	"go-di-sample/application/service"
 	"go-di-sample/infrastructure/api/jsonplaceholder"
-	"go-di-sample/interface/handlers"
+	"go-di-sample/interface/handler"
 )
-
-//TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
 
 func main() {
 	c := jsonplaceholder.NewClient()
 	s := service.NewUserService(c)
-	h := handlers.NewUserHandler(s)
+	h := handler.NewUserHandler(s)
 
 	e := echo.New()
 	e.GET("/users", h.GetUserList)
@@ -21,6 +18,3 @@ func main() {
 
 	e.Logger.Fatal(e.Start(":1234"))
 }
-
-//TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
-// Also, you can try interactive lessons for GoLand by selecting 'Help | Learn IDE Features' from the main menu.
